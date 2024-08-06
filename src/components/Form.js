50d25e36a7dd304788ee066ef06fb8506908f712
Lê Hoàng Khánh Duy <lhkhanhduy.dev@gmail.com>
2024-08-06 23:50:25 +0700

    add api contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,49 @@
 import "./Form.css"
 
 import React from 'react'
+import Swal from 'sweetalert2'
 
 const Form = () => {
+
+  //contact submit api
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "94359d3f-b2d2-4683-a60c-8a655cfe4b0e");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+        title: "Done!",
+        text: "Message sent successfully!",
+        icon: "success"
+      });
+    }
+  };
+
   return (
     <div className="form">
         <h1>Send me anything you want</h1>
-        <form>
+        <form onSubmit={onSubmit}>
             <label>Your name</label>
-            <input type="text"></input>
+            <input type="text" placeholder="Enter your name" required name="name"></input>
             <label>Email</label>
-            <input type="text"></input>
-            <label>Subject</label>
-            <input type="text"></input>
+            <input type="text" placeholder="Enter your email" required name="email"></input>
             <label>Message</label>
-            <textarea rows="6" placeholder="Type your message"></textarea>
-            <button className="btn">Send</button>
+            <textarea rows="6" placeholder="Enter your message" required name="message"></textarea>
+            <button className="btn" type="submit">Send</button>
         </form>
     </div>
   )
